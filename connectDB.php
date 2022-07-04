a86b399451f8378524b413267927bb055a1b6c20
Sukmaadi047 <91385354+Sukmaadi047@users.noreply.github.com>
2022-07-04 12:18:10 +0700

    Update connectDB.php

--- connectDB.php
+++ connectDB.php
@@ -2,8 +2,8 @@
 /* Database connection settings */
 	$servername = "localhost";
     $username = "u743710388_user";		//put your phpmyadmin username.(default is "root")
-    $password = "Rachmatte28";			//if your phpmyadmin has a password put it here.(default is "root")
-    $dbname = "u743710388_bottelegram";
+    $password = "SukmaAdi047";			//if your phpmyadmin has a password put it here.(default is "root")
+    $dbname = "RFID_MBKM_SPI_4";
     
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	
